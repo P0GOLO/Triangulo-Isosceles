28c5773d0c25c2f0e0dd8f9b05d194de3ae3bcf9
P0GOLO <pogolo@LAPTOP-BEUR4A80.localdomain>
2022-07-19 13:54:19 -0500

    Valores de cada lago guardados y HTML con JS conectados

---
+++ Triangulo.js
@@ -0,0 +1,14 @@
+// Obteniendo el cada valor independientemente
+function ladoA() {
+    const input = document.getElementById("ladoTrainguloA")
+    const valorA = input.value;
+}
+function ladoB() {
+    const input = document.getElementById("ladoTrainguloB")
+    const valorB = input.value;
+}
+function base() {
+    const input = document.getElementById("ladoBase")
+    const valorBase = input.value;
+}
+
